121cc6195acee805cfa074a17313697e453659b3
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-21 12:48:55 +0100

    asp.net core url rewrite

---
+++ Fiver.Asp.UrlRewriting/MoviesRedirectRule.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Linq;
+
+namespace Fiver.Asp.UrlRewriting
+{
+    public class MoviesRedirectRule : IRule
+    {
+        private readonly string[] matchPaths;
+        private readonly PathString newPath;
+
+        public MoviesRedirectRule(string[] matchPaths, string newPath)
+        {
+            if (matchPaths.Count() == 0)
+                throw new ArgumentException("matchPaths empty");
+
+            if (matchPaths.Where(s => !s.StartsWith("/")).Count() > 0)
+                throw new ArgumentException("matchWith values must start with /");
+
+            if (string.IsNullOrEmpty(newPath))
+                throw new ArgumentException("newPath missing");
+
+            if (!newPath.StartsWith("/"))
+                throw new ArgumentException("newPath must start with /");
+
+            this.matchPaths = matchPaths;
+            this.newPath = new PathString(newPath);
+        }
+
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            // if already redirected, skip
+            if (request.Path.StartsWithSegments(new PathString(this.newPath)))
+            {
+                return;
+            }
+
+            if (this.matchPaths.Contains(request.Path.Value))
+            {
+                var newLocation = $"{this.newPath}{request.QueryString}";
+
+                var response = context.HttpContext.Response;
+                response.StatusCode = StatusCodes.Status302Found;
+                context.Result = RuleResult.EndResponse;
+                response.Headers[HeaderNames.Location] = newLocation;
+            }
+        }
+    }
+}
--- Fiver.Asp.UrlRewriting/Startup.cs
+++ Fiver.Asp.UrlRewriting/Startup.cs
@@ -1,33 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace Fiver.Asp.UrlRewriting
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(
+            IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            var rewrite = new RewriteOptions()
+                .AddRedirect("films", "movies")
+                .AddRewrite("actors", "stars", true);
 
-            app.Run(async (context) =>
+            //var rewrite = new RewriteOptions()
+            //    .AddRedirectToHttps(302, 7229);
+
+            //var rewrite = new RewriteOptions()
+            //    .Add(new MoviesRedirectRule(
+            //            matchPaths: new[] { "/films", "/features", "/albums" },
+            //            newPath: "/movies"));
+
+            app.UseRewriter(rewrite);
+
+            app.Run(async context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var path = context.Request.Path;
+                var query = context.Request.QueryString;
+                await context.Response.WriteAsync($"New URL: {path}{query}");
             });
         }
     }
